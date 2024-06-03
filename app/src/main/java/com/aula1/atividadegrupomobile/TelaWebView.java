bd72d64939b8ae14fbca4ea27042cbe7ffeb7222
Heitor Kendi Katsuki <heitorkatsuki-ieg@instituto.ad>
2024-06-03 08:43:52 -0300

    Adicionando a classe WebView

---
+++ app/src/main/java/com/aula1/atividadegrupomobile/TelaWebView.java
@@ -0,0 +1,32 @@
+package com.aula1.atividadegrupomobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class TelaWebView extends AppCompatActivity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+//        Verificando se tem outra página em segundo plano antes de fechar o app
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
